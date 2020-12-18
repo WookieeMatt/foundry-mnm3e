b5e13ed4d38aef07835e51f69f156e4fea36a898
Cwolf-Os <cwolfterminal@gmail.com>
2020-12-18 12:48:34 -0600

    Editor not scaling but getting there

--- module/sheets/MM3ItemSheet.js
+++ module/sheets/MM3ItemSheet.js
@@ -1,4 +1,13 @@
 export default class MM3ItemSheet extends ItemSheet {
+
+    static get defaultOptions(){
+        return mergeObject(super.defaultOptions, {
+            width: 530,
+            height: 340,
+            classes: ["mnm3e", "sheet", "item"]
+        });
+    }
+
     get  template() {
         return `systems/mnm3e/templates/sheets/${this.item.data.type}-sheet.hbs`;
     }
