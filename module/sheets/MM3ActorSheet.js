6e5bdcb1939a4bbf3ae821452225419e5e16e1ba
Cwolf-Os <cwolfterminal@gmail.com>
2020-12-21 00:03:06 -0600

    Hero css size completed

--- module/sheets/MM3ActorSheet.js
+++ module/sheets/MM3ActorSheet.js
@@ -3,6 +3,13 @@ export default class MM3ActorSheet extends ActorSheet {
         return `systems/mnm3e/templates/sheets/${this.actor.data.type}-sheet.hbs`;
     }
 
+    static get defaultOptions(){
+        return mergeObject(super.defaultOptions, {
+            width: 750,
+            height: 600,
+            classes: ["mnm3e", "sheet", "hero"]
+        });
+    }
     getData() {
         const data = super.getData();
 
