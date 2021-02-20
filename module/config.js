645af38e264ea04172bdfb9d0df2f04d242757b8
WOOKIEE-DESKTOP\mriml <mrimler1983@gmail.com>
2021-02-19 20:29:16 -0700

    READ DESCRIPTION Working on skills, struggling cannot calculate yet, READ DESCRIPTION
    I'm working on skills calculating, i added skills to the Config.js, I made actor.js, modified sheet slightly to match what i am trying to do

--- module/config.js
+++ module/config.js
@@ -11,3 +11,21 @@ mnm3e.armorCategories = {
     modern: "mnm3e.armor.categories.modern",
     shields: "mnm3e.armor.categories.shields",
 }
+
+mnm3e.skills = {
+    "acrobatics": "mnm3e.hero.skills.acrobatics",
+    "athletics": "mnm3e.hero.skills.athletics",
+    "close-combat": "mnm3e.hero.skills.close-combat",
+    "deception": "mnm3e.hero.skills.deception",
+    "expertise": "mnm3e.hero.skills.expertise",
+    "insight": "mnm3e.hero.skills.insight",
+    "intimidation": "mnm3e.hero.skills.intimidation",
+    "investigation": "mnm3e.hero.skills.investigation",
+    "ranged-combat": "mnm3e.hero.skills.ranged-combat",
+    "sleight-of-hand": "mnm3e.hero.skills.sleight-of-hand",
+    "stealth": "mnm3e.hero.skills.stealth",
+    "technology": "mnm3e.hero.skills.technology",
+    "treatment": "mnm3e.hero.skills.treatment",
+    "vehicles": "mnm3e.hero.skills.vehicles"
+
+}
